59c29b2f67578d0dc143066930c11c2f35a188c3
colderry <c0l4e6r7@gmail.com>
2023-04-21 15:29:02 +0600

    Add `Routing` to the app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import NavbarComponent from "./components/Shared/NavbarComponent";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./Signup";
+
 function App() {
   return (
-    <div className="has-text-centered mt-6">
-        <h1 className="is-size-3">Henlo World</h1>
-    </div>
+    <>
+        <BrowserRouter>
+        <NavbarComponent />
+          <Switch>
+            <Routes></Routes>
+          </Switch>
+        </BrowserRouter>
+    </>
+  );
+}
+
+function Routes() {
+  return (
+    <>
+      <Route exact path="/"><h1 className="has-text-centered mt-6 is-size-3">Henlo World</h1></Route>
+      <Route path="/login"><Login /></Route>
+      <Route path="/signup"><Signup /></Route>
+    </>
   );
 }
 
